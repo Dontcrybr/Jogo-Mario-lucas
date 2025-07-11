822a8c8c6953fde38ba0bbfc24a2d18ab6122429
migueldev <miguel11psilva@gmail.com>
2025-07-11 19:28:35 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const resetButton = document.querySelector('.reset');
+
+resetButton.addEventListener('click', () => {
+    location.reload();
+});
+
+
+
+const jump = () => {
+
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+         mario.classList.remove('jump');
+
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    
+    if (pipePosition <= 240 && pipePosition > 0 && marioPosition < 160) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+         mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = './images/game-over.png';
+        mario.style.width = '150px';
+        mario.style.marginLeft = '100px';
+
+        resetButton.style.display = 'block'; 
+
+        clearInterval(loop);
+    }
+
+},10)
+
+document.addEventListener('keydown', jump)
+document.addEventListener('touchstart', jump);
+document.addEventListener('click', jump);
